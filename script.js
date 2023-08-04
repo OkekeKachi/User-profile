4d0375f0c210539c2ebaa51fe352cf60f7de2897
OkekeKachi <141140531+OkekeKachi@users.noreply.github.com>
2023-08-04 10:13:38 +0100

    adding of javascript to html

---
+++ script.js
@@ -0,0 +1,5 @@
+function generateName(){
+    var aboutMe = "Felix is an average web developer currently doing an internship at danisoft"            
+    document.getElementById("nameDisplay").textContent=aboutMe;//Display the name
+}
+   
